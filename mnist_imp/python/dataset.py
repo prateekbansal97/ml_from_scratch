61f99a66948a05e7e99c39eec898a9ed1aeedc0a
prateek <prateekbansal97@gmail.com>
2025-08-01 15:06:21 -0500

    Added DataLoader Class

--- mnist_imp/python/dataset.py
+++ mnist_imp/python/dataset.py
@@ -26,3 +26,32 @@ class dataset:
 
     def __getitem__(self, idx):
         return self.features[idx], self.labels[idx]
+
+class DataLoader:
+    def __init__(self, dataset, batch_size=1):
+        if not isinstance(dataset, Dataset):
+            raise ValueError("dataset should be of class Dataset")
+        self.dataset = dataset
+        self.batch_size = batch_size
+        self.indices = np.arange(len(dataset))
+        np.random.shuffle(self.indices)
+        self.n_batches = int(np.ceil(len(self.dataset) / self.batch_size))
+
+    def __len__(self):
+        return self.n_batches
+
+    def get_batch(self, idx):
+        findex = idx*self.batch_size
+        lindex = min(findex + self.batch_size, len(self.dataset))
+        local_batch_size = lindex - findex
+        feature_batch = self.dataset.features[self.indices[findex:lindex]].reshape(local_batch_size, -1)
+        labels_batch = self.dataset.labels[self.indices[findex:lindex]]
+        return feature_batch, labels_batch
+
+    def __getitem__(self, idx):
+        return self.get_batch(idx)
+
+    def __iter__(self):
+        np.random.shuffle(self.indices)
+        for i in range(self.num_batches):
+            yield self.get_batch(i)
